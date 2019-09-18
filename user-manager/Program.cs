d09c38fbfac25231b6ca60f6c573c4e2f7412ff0
jonta052 <55479519+jonta052@users.noreply.github.com>
2019-09-18 11:34:14 +0200

    Update Program.cs
    Added main menu

--- user-manager/Program.cs
+++ user-manager/Program.cs
@@ -6,7 +6,23 @@ namespace user_manager
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Users!");
+            
+            
+            showMainMenu();
+
+        }
+        static int showMainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Welcome to user manager");
+            Console.WriteLine("1. Create user");
+            Console.WriteLine("2. Show all users");
+            Console.WriteLine("3. Mail user");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("Select an option");
+
+            int input = int.Parse(Console.ReadLine());
+            return input;
         }
     }
 }
